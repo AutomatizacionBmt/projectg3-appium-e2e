e96f9b7863ce6b38aa4499a16cc21cf33e3b3b1b
Jose Hurtado <josehumberto17@gmail.com>
2021-01-30 11:50:11 -0500

    Agregar nuevo test swipe

--- src/test/java/com/company/features/AndroidNativeDemosTest.java
+++ src/test/java/com/company/features/AndroidNativeDemosTest.java
@@ -2,6 +2,7 @@ package com.company.features;
 
 import com.company.base.BaseTest;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.LongPressOptions;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
@@ -65,6 +66,46 @@ public class AndroidNativeDemosTest extends BaseTest {
         List<WebElement> listTitles = driver.findElementsById("android:id/title");
 
         Assert.assertTrue(listTitles.get(1).isDisplayed());
+    }
+
+    @Test
+    public void testSwipeDemoNexusAndroid7(){
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setCapability("automationName", "UiAutomator2");
+        cap.setCapability("platformName","Android");
+        cap.setCapability("platformVersion","7.0");
+        cap.setCapability("deviceName","Nexus5Android7");
+        cap.setCapability("newComandTimeout", "200");
+
+        //Dispositivo Virtual
+        cap.setCapability("avd","Nexus5Android7");
+        cap.setCapability("avdArgs","-port 5557");
+
+        //Dispositivo Real
+        //cap.setCapability("udid","emulator-5554");
+
+        cap.setCapability("app", "/Users/jhumbertoh/Proyectos/Publicos/projectg3-appium-e2e/resources/apk/ApiDemos-debug.apk");
+
+        configAppiumDriver(cap);
+
+        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Views\"]").click();
+        driver.findElementByAccessibilityId("Date Widgets").click();
+
+        AndroidDriver androidDriver = (AndroidDriver)driver;
+        androidDriver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
+
+        driver.findElementByXPath("//*[@content-desc=\"9\"]").click();
+
+        TouchAction touch = new TouchAction(driver);
+
+        WebElement first = driver.findElementByXPath("//*[@content-desc=\"15\"]");
+        WebElement second = driver.findElementByXPath("//*[@content-desc=\"45\"]");
+
+        touch.longPress(longPressOptions()
+                .withElement(element(first))
+                .withDuration(Duration.ofSeconds(2)))
+        .moveTo(element(second)).release().perform();
     }
 
 
